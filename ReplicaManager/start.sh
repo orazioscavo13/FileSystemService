e3657bed5d33a48d8895c14c1a3d19ed8942a6e7
orazioscavo13 <orazio.scavo@gmail.com>
2019-01-04 04:34:39 +0100

    Created branch docker

--- ReplicaManager/start.sh
+++ ReplicaManager/start.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 
 service mongodb start
+echo 'poiuytrew'
+mongod --dbpath /dbData &
+echo 'ooooooooo'
 /usr/local/glassfish4/bin/asadmin start-domain
 /usr/local/glassfish4/bin/asadmin deploy /ReplicaManager-1.0-SNAPSHOT.war
 /usr/local/glassfish4/bin/asadmin stop-domain
