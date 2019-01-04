8018fa99d0a6aabd3d5d0ff20df859f2d35126e2
Taletex <alessandromessina95@hotmail.com>
2019-01-04 13:21:12 +0100

    modified databasemanager frontend

--- DatabaseManager/src/main/webapp/mainController.js
+++ DatabaseManager/src/main/webapp/mainController.js
@@ -14,11 +14,11 @@ app.controller('mainCtrl', function($scope, $http, $httpParamSerializerJQLike) {
     $scope.baseUrl = "http://localhost:43637/DatabaseManager-1.0-SNAPSHOT/webresources/mongodb/";
 
 
-    /* === DIRECTORIES REST === */
+    /* === COLLECTION REST === */
     $scope.readCollection = function(collectionName) {
         if(collectionName != null) {
             $http.get($scope.baseUrl + "collections/" + collectionName).then(function(resp) {
-                $scope.queryOutput = resp.data;
+                $scope.queryOutput = syntaxHighlight(JSON.stringify(resp.data, undefined, 4));
                 console.log(resp);
             });            
         }
@@ -28,7 +28,7 @@ app.controller('mainCtrl', function($scope, $http, $httpParamSerializerJQLike) {
         
         if(collectionName != null) {
             $http.get($scope.baseUrl + "collections/" + collectionName + "/lastCommittedDocument").then(function(resp) {
-                $scope.queryOutput = resp.data;
+                $scope.queryOutput = syntaxHighlight(JSON.stringify(resp.data, undefined, 4));
                 console.log(resp);
             });            
         }
@@ -37,10 +37,30 @@ app.controller('mainCtrl', function($scope, $http, $httpParamSerializerJQLike) {
     $scope.insertDocument = function(collectionName, directory, cycle, meanAdd, meanDownload, stdDevAdd, stdDevDownload, state) {
         if(collectionName != null) {
             $http.post($scope.baseUrl + "collections/write", $httpParamSerializerJQLike({collection_name: collectionName, directory: directory, cycle: cycle, mean_add: meanAdd, mean_download: meanDownload, stddev_add: stdDevAdd, stddev_download: stdDevDownload, state: state, timestamp: moment().format("yyyy/MM/dd - HH:mm:ss")}), {headers:{'Content-Type': 'application/x-www-form-urlencoded'}}).then(function(resp) {
-                $scope.queryOutput = resp.data;
+                $scope.queryOutput = syntaxHighlight(JSON.stringify(resp.data, undefined, 4));
                 console.log(resp);
             });    
         }
     };
+    
+    /* Others */
+    function syntaxHighlight(json) {
+        json = json.replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;');
+        return json.replace(/("(\\u[a-zA-Z0-9]{4}|\\[^u]|[^\\"])*"(\s*:)?|\b(true|false|null)\b|-?\d+(?:\.\d*)?(?:[eE][+\-]?\d+)?)/g, function (match) {
+            var cls = 'number';
+            if (/^"/.test(match)) {
+                if (/:$/.test(match)) {
+                    cls = 'key';
+                } else {
+                    cls = 'string';
+                }
+            } else if (/true|false/.test(match)) {
+                cls = 'boolean';
+            } else if (/null/.test(match)) {
+                cls = 'null';
+            }
+            return '<span class="' + cls + '">' + match + '</span>';
+        });
+    }
     
 });
